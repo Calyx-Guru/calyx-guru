66d21c848cdbe5f4ff8e5746f126ec57cacb3af4
Chung Nguyen <nguyentruongchung@gmail.com>
2026-05-25 22:40:53 +0700

    update new icons

--- src/assets/images/kau-cim/index.ts
+++ src/assets/images/kau-cim/index.ts
@@ -2,7 +2,6 @@ export { default as careerIcon } from "./career-icon.png";
 export { default as kaucimCollection } from "./collection.png";
 export { default as kaucimIcon } from "./icon.png";
 export { default as loveIcon } from "./love-icon.png";
-export { default as kaucimOrb } from "./orb-1.png";
 export { default as kaucimOrbVortex } from "./vortex-4.gif";
 export { default as wealthIcon } from "./wealth-icon.png";
 
